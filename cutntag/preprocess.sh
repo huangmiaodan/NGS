61678943b190564663a078e08a71744ccd6838fa
huangmiaodan <1625110253@qq.com>
2026-03-09 14:40:37 +0800

    preprocess.sh v0.2, bowtie2 parameters modified

--- cutntag/preprocess.sh
+++ cutntag/preprocess.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #SBATCH -J cutntag
 #SBATCH -N 1
-#SBATCH --cpus-per-task=36
-#SBATCH -p node
+#SBATCH --exclusive
+#SBATCH -p #node#
 #SBATCH -D path
 #SBATCH -o cutntag_%j.log
 #SBATCH -e cutntag_%j.err
 
 set -eo pipefail
 
-dir=data/dir
+dir=#data/dir#
 cd $dir
-hg38=genome/dir
-THREADS_PER_SAMPLE=12
+hg38=#genome/dir#
+THREADS_PER_SAMPLE=24
 
 
 mkdir -p trimmed bam bw
 
 echo "Running on $(hostname)"
-echo "Total CPUs: $SLURM_CPUS_PER_TASK"
 
-parallel -j 3 '
+parallel -j 12 '
 sample=$(basename {= s/_R1.fq.gz// =})
 
 echo "=== Processing ${sample} ==="
@@ -32,12 +31,12 @@ trim_galore \
 --paired raw/${sample}_R1.fq.gz raw/${sample}_R2.fq.gz \
 -o trimmed/
 
-bowtie2 \
+bowtie2 --very-sensitive --local --no-unal --no-mixed --no-discordant -I 10 -X 700 \
 -p '"$THREADS_PER_SAMPLE"' \
 -1 trimmed/${sample}_R1_val_1.fq.gz \
 -2 trimmed/${sample}_R2_val_2.fq.gz \
 -x '"$hg38"' | \
-/Share/app/samtools-1.14/samtools sort \
+samtools sort \
 -@ '"$THREADS_PER_SAMPLE"' \
 -o bam/${sample}.bam
 
